65333e8ae76685259741e7fd26629e28b74995fd
Roy Eldar <royeldar@hotmail.com>
2024-11-15 20:30:53 +0200

    Implement basic calling protocol

--- static/script.js
+++ static/script.js
@@ -117,4 +117,322 @@
 		return JSON.parse(msg);
 	}
 
+	let pollingId = null;
+
+	// Handle messages by polling the server until there is an error
+	async function pollServer() {
+		while ((msg = await receiveMessage()) !== null) {
+			try {
+				await handleMessage(msg);
+			} catch (e) {
+				console.error(e)
+			}
+		}
+	}
+
+	// 100ms
+	const pollingInterval = 100;
+
+	// Start polling server
+	function startPolling() {
+		console.log('Starting to poll server');
+		pollingId = setInterval(pollServer, pollingInterval);
+	}
+
+	// Stop polling server
+	function stopPolling() {
+		console.log('Stopping polling server');
+		clearInterval(pollingId);
+	}
+
+	// Handle a single message that was received
+	async function handleMessage(msg) {
+		const remoteUsername = getRemoteUsername();
+		if (msg.name !== remoteUsername) {
+			console.warn(`Mismatching username (${msg.name})`);
+			return;
+		}
+		switch (msg.type) {
+		case 'call-offer':
+			await handleCallOfferMessage(msg);
+			break;
+		case 'call-answer':
+			await handleCallAnswerMessage(msg);
+			break;
+		case 'call-acknowledgement':
+			await handleCallAcknowledgementMessage(msg);
+			break;
+		case 'call-hangup':
+			await handleCallHangupMessage(msg);
+			break;
+		default:
+			console.warn(`Unknown message type (${msg.type})`);
+		}
+	}
+
+	let side = null;
+	let ourId = null;
+	let theirId = null;
+	let sessionId = null;
+
+	// Generate a unique random id
+	function generateUniqueId() {
+		return Math.floor(Math.random() * 1000000000);
+	}
+
+	// Construct a session id from the caller/callee id's
+	function createSessionId() {
+		switch (side) {
+		case 'caller':
+			sessionId = ourId + '_' + theirId;
+			break;
+		case 'callee':
+			sessionId = theirId + '_' + ourId;
+			break;
+		}
+		console.log(`Created session (id = ${sessionId})`);
+	}
+
+	// Send a call offer message
+	async function sendCallOfferMessage() {
+		console.log('Sending a call offer');
+
+		ourId = generateUniqueId();
+		await sendMessage({
+			name: localUsername,
+			type: 'call-offer',
+			callerId: ourId
+		});
+	}
+
+	// Handle a call offer message
+	async function handleCallOfferMessage(msg) {
+		console.log('Got a call offer');
+
+		if (side !== 'callee') {
+			console.warn(`Wrong side for call offer (${side})`);
+		} else if (peerConnection !== null) {
+			console.warn('Got a call offer in the middle of an ongoing call');
+		} else {
+			if (theirId !== null) {
+				console.info('Got another call offer');
+			}
+			theirId = msg.callerId;
+
+			// Send call answer to the other user
+			await sendCallAnswerMessage();
+		}
+	}
+
+	// Send a call answer message
+	async function sendCallAnswerMessage() {
+		console.log('Sending a call answer');
+
+		ourId = generateUniqueId();
+		await sendMessage({
+			name: localUsername,
+			type: 'call-answer',
+			callerId: theirId,
+			calleeId: ourId
+		});
+	}
+
+	// Handle a call answer message
+	async function handleCallAnswerMessage(msg) {
+		console.log('Got a call answer');
+
+		if (side !== 'caller') {
+			console.warn(`Wrong side for call answer (${side})`);
+		} else if (peerConnection !== null) {
+			console.warn('Got a call answer in the middle of an ongoing call');
+		} else if (msg.callerId !== ourId) {
+			console.warn('Incorrect caller id');
+		} else if (theirId !== null) {
+			console.warn('Did not expect to get a call answer');
+		} else {
+			theirId = msg.calleeId;
+
+			// Construct session id
+			createSessionId();
+
+			// Create a peer connection
+			createPeerConnection();
+
+			// Send call acknowledgement
+			await sendCallAcknowledgementMessage();
+		}
+	}
+
+	// Send a call acknowledgement message
+	async function sendCallAcknowledgementMessage() {
+		console.log('Sending a call acknowledgement');
+
+		await sendMessage({
+			name: localUsername,
+			type: 'call-acknowledgement',
+			calleeId: theirId
+		});
+	}
+
+	// Handle a call acknowledgement message
+	async function handleCallAcknowledgementMessage(msg) {
+		console.log('Got a call acknowledgement')
+
+		if (side !== 'callee') {
+			console.warn(`Wrong side for call acknowledgement (${side})`);
+		} else if (peerConnection !== null) {
+			console.warn('Got a call acknowledgement in the middle of an ongoing call');
+		} else if (msg.calleeId !== ourId) {
+			console.warn('Incorrect callee id');
+		} else {
+			// Construct session id
+			createSessionId();
+
+			// Create a peer connection
+			createPeerConnection();
+		}
+	}
+
+	// Send a call hangup message
+	async function sendCallHangupMessage() {
+		console.log('Sending a call hangup');
+
+		await sendMessage({
+			name: localUsername,
+			type: 'call-hangup',
+			sessionId: sessionId
+		});
+	}
+
+	// Handle a call hangup message
+	async function handleCallHangupMessage(msg) {
+		console.log('Got a call hangup');
+
+		if (peerConnection === null) {
+			console.warn('Got a call hangup without an ongoing call');
+		} else if (msg.sessionId !== sessionId) {
+			console.warn('Incorrect session id');
+		} else {
+			// Destroy the peer connection
+			destroyPeerConnection();
+
+			// There is no session anymore
+			sessionId = null;
+			ourId = null;
+			theirId = null;
+		}
+	}
+
+	// Define a button for initiating a call
+	const initiateCallButton = document.querySelector('button#initiateCall');
+	initiateCallButton.addEventListener('click', async (event) => {
+		const remoteUsername = getRemoteUsername();
+		if (localUsername === null || localUsername !== getLocalUsername()) {
+			alert('Our username is not set');
+		} else if (remoteUsername === '') {
+			alert('Their username is not set');
+		} else if (localUsername === remoteUsername) {
+			alert('Cannot start a call with ourselves');
+		} else {
+			// Disable and enable some elements
+			localUsernameInput.disabled = true;
+			setUsernameButton.disabled = true;
+			registerUsernameButton.disabled = true;
+			unregisterUsernameButton.disabled = true;
+			remoteUsernameInput.disabled = true;
+			initiateCallButton.disabled = true;
+			receiveCallButton.disabled = true;
+			endCallButton.disabled = false;
+
+			// The side that initiates the call is the caller
+			side = 'caller';
+
+			// Start receiving messages intended for our user
+			startPolling();
+
+			// Send call offer to the other user
+			await sendCallOfferMessage();
+		}
+	});
+
+	// Define a button for receiving a call
+	const receiveCallButton = document.querySelector('button#receiveCall');
+	receiveCallButton.addEventListener('click', async (event) => {
+		const remoteUsername = getRemoteUsername();
+		if (localUsername === null || localUsername !== getLocalUsername()) {
+			alert('Our username is not set');
+		} else if (remoteUsername === '') {
+			alert('Their username is not set');
+		} else if (localUsername === remoteUsername) {
+			alert('Cannot start a call with ourselves');
+		} else {
+			// Disable and enable some elements
+			localUsernameInput.disabled = true;
+			setUsernameButton.disabled = true;
+			registerUsernameButton.disabled = true;
+			unregisterUsernameButton.disabled = true;
+			remoteUsernameInput.disabled = true;
+			initiateCallButton.disabled = true;
+			receiveCallButton.disabled = true;
+			endCallButton.disabled = false;
+
+			// The side that answers the call is the callee
+			side = 'callee';
+
+			// Start receiving messages intended for our user
+			startPolling();
+		}
+	});
+
+	// Define a button for ending a call
+	const endCallButton = document.querySelector('button#endCall');
+	endCallButton.addEventListener('click', async (event) => {
+		// If there is an ongoing call, destroy the peer connection and send hangup
+		if (peerConnection !== null) {
+			// Destroy the peer connection
+			destroyPeerConnection();
+
+			// Send call hangup to the other user
+			await sendCallHangupMessage();
+
+			// There is no session anymore
+			sessionId = null;
+		}
+
+		// Stop receiving messages intended for our user
+		stopPolling();
+
+		// There is no side anymore
+		side = null;
+		ourId = null;
+		theirId = null;
+
+		// Enable and disable some elements
+		localUsernameInput.disabled = false;
+		setUsernameButton.disabled = false;
+		registerUsernameButton.disabled = false;
+		unregisterUsernameButton.disabled = false;
+		remoteUsernameInput.disabled = false;
+		initiateCallButton.disabled = false;
+		receiveCallButton.disabled = false;
+		endCallButton.disabled = true;
+	});
+
+	let peerConnection = null;
+
+	// Create an RTC peer connection
+	function createPeerConnection() {
+		console.log('Creating an RTC peer connection');
+
+		peerConnection = new RTCPeerConnection();
+	}
+
+	// Destroy the RTC peer connection
+	function destroyPeerConnection() {
+		console.log('Destroying the peer connection');
+
+		peerConnection.close();
+		peerConnection = null;
+	}
+
 })();
