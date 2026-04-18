73e1f91bb77d426bb521b550750d9aa1e4b3dc92
royeldar <royeldar@hotmail.com>
2026-04-19 00:08:54 +0300

    Slightly improve turnserver cmdline args

--- server.sh
+++ server.sh
@@ -10,6 +10,10 @@
 : "${TURN_PASSWORD:=password}"
 : "${TURN_EXTRA_FLAGS:=}"
 
+TURN_DB="./turnserver.db"
+TURN_LOGFILE="./turnserver.log"
+TURN_PIDFILE="./turnserver.pid"
+
 cleanup() {
     kill $PY_PID $TURN_PID
     exit 0
@@ -21,11 +25,15 @@ python3 server.py --address "$IP_ADDRESS" --port "$PY_PORT" &
 PY_PID=$!
 
 turnserver \
+    -n \
     --listening-ip "$IP_ADDRESS" \
     --listening-port "$TURN_PORT" \
     --realm "$TURN_REALM" \
     --user "$TURN_USERNAME:$TURN_PASSWORD" \
     --lt-cred-mech \
+    --db "$TURN_DB" \
+    --log-file "$TURN_LOGFILE" \
+    --pidfile "$TURN_PIDFILE" \
     $TURN_EXTRA_FLAGS &
 TURN_PID=$!
 
